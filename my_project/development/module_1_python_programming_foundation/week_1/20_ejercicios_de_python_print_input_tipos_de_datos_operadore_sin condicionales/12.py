ec109320a56f41b77142c7de67c2335a64085a65
TonyS-dev <166175074+TonyS-dev@users.noreply.github.com>
2025-03-13 17:50:32 +0000

    modificacion 12.py

--- my_project/development/module_1_python_programming_foundation/week_1/20_ejercicios_de_python_print_input_tipos_de_datos_operadore_sin condicionales/12.py
+++ my_project/development/module_1_python_programming_foundation/week_1/20_ejercicios_de_python_print_input_tipos_de_datos_operadore_sin condicionales/12.py
@@ -1,1 +1,3 @@
 #Pide al usuario su edad y muestra un mensaje que diga si su edad es mayor que 18 (usar operadores de comparación, sin condicionales).
+edad = int(input("Por favor, ingresa tu edad: "))
+print("¿Es mayor de 18 años?:", edad > 18)
